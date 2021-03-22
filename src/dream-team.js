6fa08b2da4c97b8f10ed5b9b7e9f3ee77b7058f7
Dina Shchobova <dina.shchobova@gmail.com>
2021-03-23 01:21:57 +0300

    feat: count-cat, dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,23 @@
-const CustomError = require("../extensions/custom-error");
+//const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports =
+function createDreamTeam(members) {
+    //throw new CustomError('Not implemented');
+    // remove line with error and write your code here
+    if (Array.isArray(members)) {
+        if (members.length === 0) {
+            return null;
+        } else {
+            let firstLetters = [];
+            for (let item of members) {
+                if (typeof (item) === 'string') {
+                    item = item.trim().toUpperCase();
+                    firstLetters.push(item[0]);
+                }
+            }
+            return (firstLetters !== []) ? firstLetters.sort().join('') : false;
+        }
+    } else {return false}
+}
+
+
