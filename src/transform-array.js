04779c899465b79aaee2b81479d565df7cd485d9
Dina Shchobova <dina.shchobova@gmail.com>
2021-03-23 23:18:05 +0300

    feat: add Transform array, What season

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,37 @@
-const CustomError = require("../extensions/custom-error");
+//const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports =
+    function transform(arr) {
+    //throw new CustomError('Not implemented');
+        let newArr = [];
+        if (arr.length === 0) {
+        return arr;
+    } else if (!Array.isArray(arr)) {
+        throw Error('Error')
+    } else {
+        let i = 0;
+        while (i < arr.length) {
+            if (arr[i] !== '--discard-next' && arr[i] !== '--discard-prev' && arr[i] !== '--double-next' && arr[i] !== '--double-prev') {
+                newArr.push(arr[i]);
+            }
+            if (arr[i] === '--discard-next' && i !== arr.length - 1 ) {
+                i++;
+            }
+            if (arr[i] === '--discard-prev' && i !== 0 && arr[i-2] !== '--discard-next') {
+                newArr.pop();
+            }
+            if (arr[i] === '--double-next' && i !== arr.length - 1) {
+                newArr.push(arr[i + 1]);
+                newArr.push(arr[i + 1]);
+                i++;
+            }
+
+            if (arr[i] === '--double-prev' && i !== 0 && arr[i-2] !== '--discard-next') {
+                newArr.push(arr[i - 1]);
+            }
+            i++;
+        }
+    }
+    return newArr;
+}
+
--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,21 @@
-const CustomError = require("../extensions/custom-error");
+//const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports =
+    function getSeason(date) {
+  //if (typeof(date) === 'string') {
+    //let date = Date.parse(date);
+ // }
+   if (date === undefined) { return 'Unable to determine the time of year!'}
+   if (!(date instanceof Date)||Object.getOwnPropertyNames(date).includes('getMonth')) {
+     throw Error('Error')
+   } else {
+     let month = date.getMonth();
+     //if (month === undefined) {return "THROWN"}
+     if (month >= 0 && month <= 1 || month === 11) { return 'winter'}
+     if (month >= 2 && month <= 4) { return 'spring'}
+     if (month >= 5 && month <= 7) { return 'summer'}
+     if (month >= 8 && month <= 10) { return 'autumn'}
+   }
+}
+
+//console.log(getSeason('hghg'))
