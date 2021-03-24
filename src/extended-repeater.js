590a3a6cc60ee0658d01af1afaaae5f3dbff4460
Dina Shchobova <dina.shchobova@gmail.com>
2021-03-25 01:24:09 +0300

    feat: add extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,26 @@
-const CustomError = require("../extensions/custom-error");
+//const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
-  
+module.exports = function repeater(str, options = {}) {
+    //throw new CustomError('Not implemented');
+    // remove line with error and write your code here
+    const repeatTimes = (options.repeatTimes === undefined) ? 1 :  options.repeatTimes;
+    const separator = (options.separator === undefined) ? '+' :  options.separator;
+    const addition = (options.addition === undefined) ? '' :  options.addition;
+    const additionRepeatTimes = (options.additionRepeatTimes === undefined) ? 1 :  options.additionRepeatTimes;
+    const additionSeparator = (options.additionSeparator === undefined) ? '|' :  options.additionSeparator;
+
+    function doAdd(add, times, sep) {
+        let array = [];
+        for (let i = 1; i <= times; i++) {
+            array.push(add+'');
+        }
+        let newStr = array.join(sep);
+        return newStr;
+    }
+
+    let allAddition = doAdd(addition, additionRepeatTimes, additionSeparator);
+
+    return doAdd(str+allAddition, repeatTimes, separator);
+
+}
+
