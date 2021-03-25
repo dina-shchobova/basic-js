2a9c100dd04dbb88fad8ef007bec95af0bbaf4b6
Dina Shchobova <dina.shchobova@gmail.com>
2021-03-26 00:35:36 +0300

    feat: add simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,47 @@
-const CustomError = require("../extensions/custom-error");
-
+//const CustomError = require("../extensions/custom-error");
+let ghi_met=false;
 const chainMaker = {
   getLength() {
-    throw new CustomError('Not implemented');
+    //throw new CustomError('Not implemented');
     // remove line with error and write your code here
+    return this.chain.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
+    //throw new CustomError('Not implemented');
     // remove line with error and write your code here
+    if(value==='GHI'&&!ghi_met){
+      ghi_met=true;
+      this.chain=[];
+    }
+    this.chain.push(`( ${value} )`);
+    return this
+
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
+    //throw new CustomError('Not implemented');
     // remove line with error and write your code here
+    if(this.chain[position-1]===undefined){
+      throw Error('ebanavrot')
+    }
+    this.chain.splice(position-1, 1);
+    return this
+
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
+    //throw new CustomError('Not implemented');
     // remove line with error and write your code here
+    this.chain.reverse();
+    return this
+
   },
   finishChain() {
-    throw new CustomError('Not implemented');
+    //throw new CustomError('Not implemented');
     // remove line with error and write your code here
-  }
+    let lastChain = this.chain.join('~~');
+    this.chain=[];
+    return lastChain;
+  },
+  chain: []
 };
 
 module.exports = chainMaker;
