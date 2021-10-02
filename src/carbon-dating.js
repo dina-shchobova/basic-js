e752e245ff0f0b3bf89187c7334cc614f22dd7f0
Dina Shchobova <dina.shchobova@gmail.com>
2021-10-02 11:14:59 +0300

    fix: solved problems 1-5

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -6,18 +6,32 @@ const HALF_LIFE_PERIOD = 5730;
 /**
  * Determine the age of archeological find by using
  * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
- * 
- * @param {String} sampleActivity string representation of current activity 
+ *
+ * @param {String} sampleActivity string representation of current activity
  * @return {Number | Boolean} calculated age in years or false
  * in case of incorrect sampleActivity
  *
  * @example
- * 
+ *
  * dateSample('1') => 22387
  * dateSample('WOOT!') => false
  *
  */
-export default function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function dateSample(sampleActivity) {
+  let x = typeof(sampleActivity);
+  let y = typeof(Number(sampleActivity));
+  let regexp = /^\d+/;
+  let z = regexp.test(sampleActivity);
+
+  if ( x !== 'string' || z !== true || y !== 'number' || Number(sampleActivity) <= 0 ) {
+    return false
+  } else {
+    let k = 0.693 / HALF_LIFE_PERIOD;
+    let age = (1 / k) * Math.log(MODERN_ACTIVITY / +sampleActivity );
+    if (Math.ceil(age) < 0) {
+      return false
+    } else {
+      return Math.ceil(age);
+    }
+  }
 }
--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -2,20 +2,36 @@ import { NotImplementedError } from '../extensions/index.js';
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function repeater(str, options) {
+  const repeatTimes = (options.repeatTimes === undefined) ? 1 :  options.repeatTimes;
+  const separator = (options.separator === undefined) ? '+' :  options.separator;
+  const addition = (options.addition === undefined) ? '' :  options.addition;
+  const additionRepeatTimes = (options.additionRepeatTimes === undefined) ? 1 :  options.additionRepeatTimes;
+  const additionSeparator = (options.additionSeparator === undefined) ? '|' :  options.additionSeparator;
+
+  function doAdd(add, times, sep) {
+    let array = [];
+    for (let i = 1; i <= times; i++) {
+      array.push(add+'');
+    }
+    let newStr = array.join(sep);
+    return newStr;
+  }
+
+  let allAddition = doAdd(addition, additionRepeatTimes, additionSeparator);
+
+  return doAdd(str+allAddition, repeatTimes, separator);
 }
